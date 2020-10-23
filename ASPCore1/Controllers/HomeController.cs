b6c95207485cf5a735b0eeb847fa70911c10f276
GerardoHelguera <gerardohelguera55@gmail.com>
2020-10-23 17:19:16 -0500

    first commit

---
+++ ASPCore1/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ASPCore1.Models;
+
+namespace ASPCore1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Array array;
+            List<EmpleadosModel> Empleados = new List<EmpleadosModel>();
+            Empleados.Add(new EmpleadosModel
+            {
+
+                Dni_empleado = 13004452,
+                Nombre_empleado = "José",
+                Ape_paterno = "Morales",
+                Ape_materno = "Alvarado",
+
+            });
+            Empleados.Add(new EmpleadosModel
+            {
+
+                Dni_empleado = 12001245,
+                Nombre_empleado = "Carlos",
+                Ape_paterno = "Cordova",
+                Ape_materno = "Velázquez ",
+
+            });
+            Empleados.Add(new EmpleadosModel
+            { 
+
+               Dni_empleado = 11003457,
+               Nombre_empleado = "María",
+               Ape_paterno = "Mireles",
+               Ape_materno = "Vargas",
+            });
+
+            array = Empleados.ToArray();
+            ViewBag.Empleados = array;
+
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPCore1/Models/EmpleadosModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore1.Models
+{
+    public class EmpleadosModel
+    {
+        public int Dni_empleado { get; set; }
+        public string Nombre_empleado { get; set; }
+        public string Ape_paterno { get; set; }
+        public string Ape_materno { get; set; }
+       
+    }
+}
